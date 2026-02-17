147efa0a0b597c0069eb65e2e708c361532b98cb
Teodor Potancok <teodor.potancok@gmail.com>
2026-02-17 17:13:31 +0100

    Add argument detection

--- justfile
+++ justfile
@@ -1,6 +1,6 @@
-run FILE:
+run file args="":
     #!/usr/bin/env sh
-    name="{{ file_stem(FILE) }}"
-    nasm "{{ FILE }}" -f elf64 -o "out/$name.o"
+    name="{{ file_stem(file) }}"
+    nasm "{{ file }}" -f elf64 -o "out/$name.o"
     ld "out/$name.o" -o "out/$name"
-    out/$name
+    out/$name {{ args }}
--- main.asm
+++ main.asm
@@ -4,17 +4,31 @@ section   .data
 
 %define message_text "slovakia #1", 10
 %strlen message_len  message_text
+message:  db message_text
 
-message:  db message_text      ; note the newline at the end
+%define args_text "arguments found", 10
+%strlen args_len  args_text
+args_msg:  db args_text
 
 section   .text
 
+%macro print 2 ; print ptr, len
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, %1     ; set pointer to string start
+    mov rdx, %2 + 1 ; don't skip last character
+    syscall
+%endmacro
+
 _start:
-    mov       rax, 1                  ; system call for write
-    mov       rdi, 1                  ; file handle 1 is stdout
-    mov       rsi, message            ; address of string to output
-    mov       rdx, message_len + 1    ; number of bytes
-    syscall                           ; invoke operating system to do the write
+    print message, message_len
+
+    mov rbx, [rsp]
+    cmp rbx, 1
+    je no_args
+    print args_msg, args_len
+
+no_args:
 
     mov       rax, 60                 ; system call for exit
     xor       rdi, rdi                ; exit code 0
