1ced0ff202509f7bd71e914ef7c0ccbf392c3d7d
Teodor Potancok <teodor.potancok@gmail.com>
2026-02-20 14:51:44 +0100

    Rename count.asm and improve docs

--- count.asm
+++ count.asm
@@ -16,15 +16,15 @@ total_msg  db "Total:", 10
 total_len  equ $ - total_msg
 
 out_buf    times 20 db 0     ; output buffer for printn
-           db ' '            ; 2^64 is 20 characters long
+           db ' '            ; 2^64 is 20 characters, all we need to print a number
 
 section   .bss
 
-buf        resb 256
-buf_offset dq ?
-read_ctr   dq ?
+buf        resb 256  ; input buffer for reading from file
+buf_offset dq ?      ; saved offset into buffer
+read_ctr   dq ?      ; characters left in buffer
 
-digits     dq ?
+digits     dq ?      ; character class counts for whole file
 big        dq ?
 small      dq ?
 other      dq ?
@@ -48,6 +48,7 @@ section   .text
     syscall
 %endmacro
 
+; fail code
 ; exits with user provided code
 %macro fail 1
     mov rax, 60     ; exit(2)
@@ -57,7 +58,6 @@ section   .text
 
 ; check_byte off, pat, dst
 ; checks if byte at [rax+off] is equal to pat, jump to dst if not
-; clobbers rsi
 %macro check_byte 3
     mov sil, [rax + %1]
     cmp sil, %2
@@ -67,6 +67,7 @@ section   .text
 %define O_RDONLY	00000000q ; open file read only
 
 ; open path, flags
+; opens file at path with flags
 %macro open 2
     mov rax, 2  ; open(2)
     mov rdi, %1 ; pointer to path
@@ -75,6 +76,7 @@ section   .text
 %endmacro
 
 ; close fd
+; closes file at fd
 %macro close 1
     mov rax, 3  ; close(2)
     mov rdi, %1 ; fd to close
@@ -82,6 +84,7 @@ section   .text
 %endmacro
 
 ; read fd, ptr, len
+; reads len bytes from fd into ptr
 %macro read 3
     mov rax, 0
     mov rdi, %1
@@ -90,34 +93,35 @@ section   .text
     syscall
 %endmacro
 
-; print number in rdi to stdout
+; prints number in rdi to stdout
+; output is unsigned decimal followed by 1 space
 printn:
     mov rax, rdi                  ; number to print
     mov rdx, 0                    ; clear out for division
-    mov rsi, out_buf + 19         ; pointer to current character
-    mov r8, 10                    ; constant 10
-    mov r9, 0                     ; chars written
+    mov rsi, out_buf + 19         ; pointer to current character, start at last digit in buffer
+    mov r8, 10                    ; constant 10 for division
+    mov r9, 0                     ; number of digits written
 
 printn_loop:
     cmp rax, 10                   ; check if on first digit (last processed)
-    jl  printn_end
+    jl  printn_end                ; no more divisions, jump to end
 
     div r8                        ; divide rax by 10
-    mov byte [rsi], dl
-    add byte [rsi], 48  ; convert to ascii digit
+    mov byte [rsi], dl            ; write remainder (always < 10) to buffer
+    add byte [rsi], 48            ; convert to ascii digit
     mov rdx, 0                    ; clear out remainder
 
-    dec rsi
-    inc r9
+    dec rsi                       ; move next digit position back 1
+    inc r9                        ; increment number of digits
     jmp printn_loop
 printn_end:
-    mov byte [rsi], al            ; print last digit
-    add byte [rsi], 48
+    mov byte [rsi], al            ; write first digit to buffer
+    add byte [rsi], 48            ; stored in rax because we skipped division
     inc r9
 
-    mov r8, r9                    ; max length, 20 digits + space
-    add r8, 2
-    print rsi, r8
+    mov r8, r9                    ; get number of digits in output
+    add r8, 2                     ; add 1 for final space
+    print rsi, r8                 ; print out result
 
     ret
 
@@ -131,24 +135,25 @@ _start:
 
     mov rbx, [rsp]                ; check number of arguments
     cmp rbx, 1
-    je no_args                    ; print message if argc > 1
+    je no_args                    ; exit if argc == 1 (no arguments)
 
-    mov rax, [rsp+16]             ; argv[1]
+    mov rax, [rsp + 16]           ; load address of argv[1]
 
     check_byte 0, '-', not_help   ; check if the argument is '-h'
     check_byte 1, 'h', not_help
     check_byte 2, 0, not_help
 
-    print help_msg, help_len
+    print help_msg, help_len      ; user needs help, print it and exit
     exit
 
 not_help:
-    open [rsp+16], O_RDONLY
+    open [rsp + 16], O_RDONLY ; open input file as read only
 
-    bt  rax, 0    ; check if return value is positive -> success
+    bt  rax, 0         ; check if return value is positive -> success
     jc  open_ok
+
     print fail_msg, fail_len
-    fail 1
+    fail 1             ; open returned error, exit with code 1
 
 open_ok:
     mov rbx, rax
@@ -160,16 +165,17 @@ read_buf:
     cmp rax, 0
     je end             ; no more bytes to read
 
-    mov rsi, 0         ; offset into buf
+    mov rsi, 0         ; clear offset before loop
 
 read_char:
     cmp byte [buf + rsi], 10   ; is newline?
-    jne same_line
+    jne same_line              ; skip printing counts if same line
 
-    ; print line counts and add to totals
+    ; save offset and read counter
     mov [buf_offset], rsi
     mov [read_ctr], rdi
 
+    ; add line counts to totals
     add [digits], r_line_digits
     add [big], r_line_big
     add [small], r_line_small
@@ -177,6 +183,7 @@ read_char:
 
     mov rbp, rsp            ; save stack pointer for argc/argv
 
+    ; print all line counts
     mov rdi, r_line_digits
     call printn
     mov rdi, r_line_big
@@ -188,57 +195,65 @@ read_char:
 
     mov rsp, rbp            ; restore stack pointer
 
-    ; clear line registers
+    ; clear line count registers
     xor r_line_digits, r_line_digits
     xor r_line_big, r_line_big
     xor r_line_small, r_line_small
     xor r_line_other, r_line_other
 
-    mov byte [out_buf], 10 ; new line
+    mov byte [out_buf], 10  ; start new line
     print out_buf, 1
 
+    ; restore offset and read counter
     mov rsi, [buf_offset]
     mov rdi, [read_ctr]
 
-    jmp loop_end
+    ; skip adding character to total
+    jmp loop_tail
 
 same_line:
-    cmp byte [buf + rsi], 47 ; control chars
+    ; find out what category the character is in
+    cmp byte [buf + rsi], 47  ; '/', control and special chars
     jle is_other
-    cmp byte [buf + rsi], 57 ; digits
+    cmp byte [buf + rsi], 57  ; '9', digits
     jle is_digit
-    cmp byte [buf + rsi], 64 ; special chars
+    cmp byte [buf + rsi], 64  ; '@', special chars
     jle is_other
-    cmp byte [buf + rsi], 90 ; capital letters
+    cmp byte [buf + rsi], 90  ; 'Z', capital letters
     jle is_big
-    cmp byte [buf + rsi], 96 ; more special chars
+    cmp byte [buf + rsi], 96  ; '`', more special chars
     jle is_other
-    cmp byte [buf + rsi], 122 ; small letters
+    cmp byte [buf + rsi], 122 ; 'z', small letters
     jle is_small
+    ; other byte
     jmp is_other
 
+    ; increment line count for category
 is_digit:
     inc r_line_digits
-    jmp loop_end
+    jmp loop_tail
 
 is_big:
     inc r_line_big
-    jmp loop_end
+    jmp loop_tail
 
 is_small:
     inc r_line_small
-    jmp loop_end
+    jmp loop_tail
 
 is_other:
     inc r_line_other
 
-loop_end:
-    inc rsi               ; add to offset
-    dec rdi               ; take from read length
-    jz read_buf           ; reached end of buffer
+loop_tail:
+    inc rsi               ; add 1 to offset
+
+    dec rdi               ; take 1 from available chars
+    jz read_buf           ; reached end of buffer and need to load more
+
     jmp read_char
 
 end:
+    ; print total counts in the end
     print total_msg, total_len
 
     mov rdi, [digits]
