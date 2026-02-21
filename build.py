6a54002d6f097b22b80aa1e5a95247089581f763
Teodor Potancok <teodor.potancok@gmail.com>
2026-02-21 12:12:18 +0100

    Replace justfile with build.py

---
+++ build.py
@@ -0,0 +1,22 @@
+import glob
+import os
+import sys
+
+
+def main():
+    try:
+        os.mkdir("out")
+    except FileExistsError:
+        pass
+
+    asm_files = [f.removesuffix(".asm") for f in glob.glob("*.asm")]
+
+    for file in asm_files:
+        os.system(f"nasm {file}.asm -f elf64 -F dwarf -o out/{file}.o")
+
+    os.system(f"ld {' '.join([f'out/{f}.o' for f in asm_files])} -o out/main")
+    os.system(f"out/main {' '.join(sys.argv[1:])}")
+
+
+if __name__ == "__main__":
+    main()
--- justfile
+++
@@ -1,7 +0,0 @@
-run file *args:
-    #!/usr/bin/env sh
-    mkdir -p out/
-    name="{{ file_stem(file) }}"
-    nasm "{{ file }}" -f elf64 -F dwarf -o "out/$name.o"
-    ld "out/$name.o" -o "out/$name"
-    out/$name {{ args }}
