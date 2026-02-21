79889aa39545eacf5caae8f43a55913d4861ac3b
Teodor Potancok <teodor.potancok@gmail.com>
2026-02-21 13:02:47 +0100

    Switch to gold linker

--- build.py
+++ build.py
@@ -14,7 +14,7 @@ def main():
     for file in asm_files:
         os.system(f"nasm {file}.asm -f elf64 -F dwarf -o out/{file}.o")
 
-    os.system(f"ld {' '.join([f'out/{f}.o' for f in asm_files])} -o out/main")
+    os.system(f"gold {' '.join([f'out/{f}.o' for f in asm_files])} -o out/main")
     os.system(f"out/main {' '.join(sys.argv[1:])}")
 
 
